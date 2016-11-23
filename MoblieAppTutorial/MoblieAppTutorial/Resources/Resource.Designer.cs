e6d492123bfe061802de76ff293be204196e886e
afshid <afshid@gmail.com>
2016-11-23 10:56:08 -0500

    v1

--- MoblieAppTutorial/MoblieAppTutorial/Resources/Resource.Designer.cs
+++ MoblieAppTutorial/MoblieAppTutorial/Resources/Resource.Designer.cs
@@ -57,6 +57,46 @@ namespace MoblieAppTutorial
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050004
+			public const int button1 = 2131034116;
+			
+			// aapt resource value: 0x7f050008
+			public const int button2 = 2131034120;
+			
+			// aapt resource value: 0x7f050001
+			public const int linearLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int linearLayout2 = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int space1 = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int space2 = 2131034119;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView3 = 2131034118;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -81,6 +121,9 @@ namespace MoblieAppTutorial
 			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int PageHeader = 2130968578;
 			
 			static String()
 			{
